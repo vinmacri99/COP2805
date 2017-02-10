10d9138281c2d4c4588996e1b36b035212e2f731
Flavio Aquino <bregarulai@gmail.com>
2017-02-09 23:39:27 -0500

    Added northPanel and title into it

--- Proj2_GUI/src/searchEngineProj/UserForm.java
+++ Proj2_GUI/src/searchEngineProj/UserForm.java
@@ -15,6 +15,9 @@ public class UserForm {
 	JRadioButton andSearch, orSearch, exactPhrase;
 	
 	public UserForm() {
+		Font titleFont = new Font("Serif", Font.BOLD, 36);
+		Font regularFont = new Font("Serif", Font.PLAIN, 20);
+		
 		// to create the window frame
 		JFrame myFrame = new JFrame();
 		myFrame.setTitle("My Search Engine");
@@ -22,6 +25,18 @@ public class UserForm {
 		myFrame.setBounds(100, 100, 850, 700);
 		myFrame.setLayout(new BorderLayout());
 		myFrame.setVisible(true);
+				
+		// to create a panel for the main title
+		Container contentPane = myFrame.getContentPane();
+		JPanel northPanel = new JPanel();
+		northPanel.setBackground(Color.BLUE);
+		northPanel.setPreferredSize(new Dimension(850, 100));
+		contentPane.add(northPanel, BorderLayout.NORTH);
+		
+		// to create and add the title to north panel
+		title = new JLabel("My Search Engine");
+		title.setFont(titleFont);
+		northPanel.add(title);
 	}
 	
 	public static void main(String[] args) {
