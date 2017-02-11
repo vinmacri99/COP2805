56156e556b04f1673ab3999cae6edab62b700b0b
Flavio Aquino <bregarulai@gmail.com>
2017-02-11 00:37:21 -0500

    Created the class for the Admin GUI

---
+++ Proj2_GUI/src/searchEngineProj/MaintenanceGui.java
@@ -0,0 +1,11 @@
+/** @author Flavio Aquino February 2017
+ * 
+ *  Class responsible for creating the  Administrator
+ *  GUI.
+ */
+
+package searchEngineProj;
+
+public class MaintenanceGui {
+
+}
