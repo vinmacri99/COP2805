49d33640d48c6f29db9e750bb6f0c73922ee8b32
Sharon Tender <sharon.tender@icloud.com>
2017-02-12 18:14:19 -0500

    commit using >git commit (then writing comments in notepad++)

--- Proj2_GUI/src/searchEngineProj/searchUtils/InvertedIndex.java
+++ Proj2_GUI/src/searchEngineProj/searchUtils/InvertedIndex.java
@@ -2,4 +2,6 @@ package searchEngineProj.searchUtils;
 
 public class InvertedIndex {
     // initial commit to merge with GitHub
+    // commit2: testiing the default editor for commit comments
+    //     
 }
