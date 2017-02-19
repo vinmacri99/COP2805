007de94e15fff1e9deb862509db8d4a18e44d0f1
Flavio Aquino <bregarulai@gmail.com>
2017-02-18 22:47:55 -0500

    Created a partial table model class to populate the JTable

---
+++ Proj2_GUI/src/searchEngineProj/TableModel.java
@@ -0,0 +1,34 @@
+/** @author Flavio Aquino February 2017*/
+
+package searchEngineProj;
+
+import javax.swing.table.AbstractTableModel;
+
+public class TableModel extends AbstractTableModel{
+	
+	private static final long serialVersionUID = 1L;
+
+	private String[] columnNames = {"File Name", "Status"};
+	// TODO add the rest of the attributes 
+	
+	public int getRowCount() {
+		return 0;
+	}
+	
+	public String getColumnName(int col) {
+		return columnNames[col];
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
