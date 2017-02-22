185453254d85fedfd971f560529f8ed77e902c9a
Sharon Tender <sharon.tender@icloud.com>
2017-02-22 18:02:50 -0500

    updated GUIFrame.java file to show my (sharon) GUI

--- Proj2_GUI/src/searchEngineProj/searchUtils/GUIFrame.java
+++ Proj2_GUI/src/searchEngineProj/searchUtils/GUIFrame.java
@@ -1,149 +1,111 @@
+/** @author Flavio Aquino, Sharon Tender, Frank Castillo,
+ * Vinny Macri February 2017
+ */
+
 package searchEngineProj.searchUtils;
 
-import java.awt.EventQueue;
+import java.awt.*;
 import java.awt.GridLayout;
-import java.awt.List;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
-import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JRadioButton;
-import javax.swing.ButtonGroup;
 
 public class GUIFrame extends JFrame {
-
-	private JPanel contentPane;
-	private static final long serialVersionUID = 1L;
-	private JTextField searchField;
-	private JTextField searchTextField;
-	private final ButtonGroup buttonGroup = new ButtonGroup();
-	/**
-	 * Create the frame.
-	 */
-	
+    // Components
+    JTextField userInput = new JTextField();
+    JLabel windowTitle = new JLabel("Search Engine");    
+    JButton searchAllBtn = new JButton("Search All Words");
+    JButton searchAnyBtn = new JButton("Search Any Word");
+    JButton searchExactBtn = new JButton("Search Exact Phrase");    
+    JButton indexbtn = new JButton("Open Search Files");
+    JButton aboutbtn = new JButton("About The A-Team");
+    JButton clearbtn = new JButton("Clear");   
+   
 	public GUIFrame() {
-		setResizable(false);
+        setResizable(false);
 		setTitle("Search Engine");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 600);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(0, 1, 0, 0));
-		
-        // Search Tab controls
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		contentPane.add(tabbedPane);
-		
-		JPanel searchTab = new JPanel();
-		tabbedPane.addTab("Search", null, searchTab, null);
-		searchTab.setLayout(null);
-		
-		List resultsList = new List();
-		resultsList.setBounds(0, 98, 429, 396);
-		searchTab.add(resultsList);
-		
-		JButton btnSearch = new JButton("Search");
-		btnSearch.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				// message box showing that code ran
-				
-			}
+		setBounds(400, 400, 800, 600);
+        Color colorGrey = new Color(220,220,220);
+        Color colorGreen = new Color(204,255,153);
+        
+        // Title for window
+        JPanel pWindow = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        pWindow.add(windowTitle);
+        Font titleFont = new Font("Bodoni MT", Font.PLAIN, 44);
+        windowTitle.setFont(titleFont);
+        
+        // Search buttons
+        JPanel p1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        TitledBorder title = new TitledBorder("Select a Search Option");
+        p1.setBorder(title);
+        p1.add(searchAllBtn);
+        p1.add(searchAnyBtn);
+        p1.add(searchExactBtn);
+        
+        // search text box
+        JPanel p2 = new JPanel(new GridLayout(1,5,40,40));
+        TitledBorder inputTitle = new TitledBorder("Enter text to search");
+        p2.setBorder(inputTitle);
+        p2.add(userInput);
+        userInput.setBounds(10, 10, 100, 10);  // tried to make this height smaller, but doesn't work
+        
+        // search results text box
+        JPanel p3 = new JPanel(new GridLayout(2,1,0,0));
+        TitledBorder resultsTitle = new TitledBorder("Search Results");
+        Border lineBorder = new LineBorder(Color.ORANGE, 1);
+        resultsTitle.setBorder(lineBorder);
+        p3.setBorder(resultsTitle);
+        
+        // Maintenance/Index options and close buttons
+        JPanel p4 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        TitledBorder optionsTitle = new TitledBorder("Other Options");
+        p4.setBorder(optionsTitle);
+        p4.add(indexbtn);
+        p4.add(aboutbtn);
+        p4.add(clearbtn);
+        
+        // display all panels
+        setLayout(new GridLayout(5,1,30,0));
+        add(pWindow).setBackground(colorGreen);
+        add(p2).setBackground(colorGreen);
+        add(p1).setBackground(colorGreen);
+        add(p3).setBackground(Color.WHITE);
+        add(p4).setBackground(colorGreen);
+        
+        pack();
+        
+        // event listeners for buttons
+        searchAllBtn.addActionListener(actionEvent -> {
+			JOptionPane.showMessageDialog(null, "User Input: " + userInput.getText(), 
+					"Warning Will Robinson!", JOptionPane.OK_OPTION);
 		});
-		btnSearch.setBounds(67, 500, 90, 23);
-		searchTab.add(btnSearch);
-		
-		JButton btnSearchClear = new JButton("Clear");
-		btnSearchClear.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				// Clear search criteria and search results
-				
-			}
+        
+        searchAnyBtn.addActionListener(actionEvent -> {
+			JOptionPane.showMessageDialog(null, "User Input: " + userInput.getText(), 
+					"Warning Will Robinson!", JOptionPane.OK_OPTION);
+		});        
+        
+        searchExactBtn.addActionListener(actionEvent -> {
+			JOptionPane.showMessageDialog(null, "User Input: " + userInput.getText(), 
+					"Warning Will Robinson!", JOptionPane.OK_OPTION);
 		});
-		btnSearchClear.setBounds(167, 500, 90, 23);
-		searchTab.add(btnSearchClear);
-		
-		JButton btnClose = new JButton("Close");
-		btnClose.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				// Close program
-				
-			}
+        
+        indexbtn.addActionListener(actionEvent -> {
+			JOptionPane.showMessageDialog(null, "This will bring up the Index Window to select files for searching", 
+					"Warning Will Robinson!", JOptionPane.OK_OPTION);
 		});
-		btnClose.setBounds(267, 500, 90, 23);
-		searchTab.add(btnClose);
-		
-		JLabel lblSearchResults = new JLabel("Search Results");
-		lblSearchResults.setBounds(0, 72, 100, 20);
-		searchTab.add(lblSearchResults);
-		
-		JLabel lblSearchCriteria = new JLabel("Search Criteria");
-		lblSearchCriteria.setBounds(0, 0, 100, 20);
-		searchTab.add(lblSearchCriteria);
-		
-		searchTextField = new JTextField();
-		searchTextField.setBounds(0, 22, 429, 20);
-		searchTab.add(searchTextField);
-		searchTextField.setColumns(10);
-		
-		JRadioButton rdbtAllTerms = new JRadioButton("All of the terms");
-		rdbtAllTerms.setSelected(true);
-		buttonGroup.add(rdbtAllTerms);
-		rdbtAllTerms.setBounds(0, 42, 115, 23);
-		searchTab.add(rdbtAllTerms);
-		
-		JRadioButton rdbtAnyTerms = new JRadioButton("Any of the terms");
-		buttonGroup.add(rdbtAnyTerms);
-		rdbtAnyTerms.setBounds(139, 42, 144, 23);
-		searchTab.add(rdbtAnyTerms);
-		
-		JRadioButton rdbtExactTerms = new JRadioButton("Exact Phrase");
-		buttonGroup.add(rdbtExactTerms);
-		rdbtExactTerms.setBounds(294, 42, 109, 23);
-		searchTab.add(rdbtExactTerms);
-		
-        // Settings Tab controls 
-		JPanel adminTab = new JPanel();
-		tabbedPane.addTab("Settings", null, adminTab, null);
-		adminTab.setLayout(null);
-		
-		List fileList = new List();
-		fileList.setBounds(0, 20, 429, 474);
-		adminTab.add(fileList);
-		
-		JLabel searchableFiles = new JLabel("File List");
-		searchableFiles.setBounds(0, 0, 100, 20);
-		adminTab.add(searchableFiles);
-		
-		JButton btnBrowse = new JButton("Browse...");
-		btnBrowse.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				// Invokes file management and loads files into files list
-				
-			}
+        
+        aboutbtn.addActionListener(actionEvent -> {
+			JOptionPane.showMessageDialog(null, "The A-Team: Flavio, Frank, Vinny, and Sharon", 
+					"Warning Will Robinson!", JOptionPane.OK_OPTION);
 		});
-		btnBrowse.setBounds(118, 500, 90, 23);
-		adminTab.add(btnBrowse);
-		
-		JButton btnSettingsClear = new JButton("Clear...");
-		btnSettingsClear.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				// Clear file list
-				
-			}
-		});
-		btnSettingsClear.setBounds(218, 500, 90, 23);
-		adminTab.add(btnSettingsClear);
-	}
+        
+        clearbtn.addActionListener(actionEvent -> {
+			JOptionPane.showMessageDialog(null, "This will clear text area.", 
+					"Warning Will Robinson!", JOptionPane.OK_OPTION);
+		});         
+   	}
 }
